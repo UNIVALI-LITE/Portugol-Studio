ef793637510424457bc0e079de73abe67958fc72
Fillipi Pelz <fillipi.pelz@gmail.com>
2011-08-04 20:46:57 -0300

    modificando estrutura de diretórios

--- src/PortugolNucleo/src/br/univali/portugol/nucleo/asa/NoPare.java
+++
@@ -1,6 +0,0 @@
-package br.univali.portugol.nucleo.asa;
-
-public final class NoPare extends NoBloco
-{
-
-}
--- src/PortugolNucleo/src/br/univali/portugol/nucleo/iu/Saida.java
+++
@@ -1,9 +0,0 @@
-package br.univali.portugol.nucleo.iu;
-
-
-
-public interface Saida
-{
-	public void limpar();
-	public void imprimir(String valor);
-}
