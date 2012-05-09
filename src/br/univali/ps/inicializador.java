de921e77a1cd757891a53deda0dab4d5e9fdea60
Luiz Fernando Noschang <noschang@univali.br>
2012-05-09 03:04:38 -0300

    Atualizando os fontes do projeto

--- src/br/univali/ps/inicializador.java
+++ src/br/univali/ps/inicializador.java
@@ -1,12 +1,17 @@
 package br.univali.ps;
 
-
 import br.univali.ps.nucleo.ExcecaoAplicacao;
 import br.univali.ps.nucleo.PortugolStudio;
+import com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-public class inicializador
+/**
+ * 
+ * @author Luiz Fernando Noschang
+ * 
+ */
+public final class Inicializador
 {
     public static void main(String argumentos[])
     {
@@ -14,31 +19,39 @@ public class inicializador
         {
             PortugolStudio portugolStudio = PortugolStudio.getInstancia();
             portugolStudio.setDepurando(isDepurando(argumentos));
+
+            try
+            {
+                UIManager.setLookAndFeel(new NimbusLookAndFeel());
+            }
+            catch (UnsupportedLookAndFeelException usException)
+            {
+            }
             
-            try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (UnsupportedLookAndFeelException usException) {}            
             portugolStudio.iniciar();
         }
         catch (Exception excecao)
         {
-            excecao.printStackTrace();
-            String mensagem = "O PortugolStudio encontrou um erro desconhecido e precisa ser fechado:\n" + excecao.getMessage();                    
-            ExcecaoAplicacao excecaoAplicacao = new ExcecaoAplicacao(mensagem, excecao, ExcecaoAplicacao.Tipo.ERRO);
+            excecao.printStackTrace(System.out);
             
+            String mensagem = "O PortugolStudio encontrou um erro desconhecido e precisa ser fechado:\n" + excecao.getMessage();
+            ExcecaoAplicacao excecaoAplicacao = new ExcecaoAplicacao(mensagem, excecao, ExcecaoAplicacao.Tipo.ERRO);
+
             PortugolStudio.getInstancia().getTratadorExcecoes().exibirExcecao(excecaoAplicacao);
             System.exit(0);
         }
     }
-    
+
     private static boolean isDepurando(String[] argumentos)
     {
-        for (String argumento: argumentos)
+        for (String argumento : argumentos)
         {
             if (argumento.equals("-debug"))
+            {
                 return true;
+            }
         }
-        
+
         return false;
     }
 }
