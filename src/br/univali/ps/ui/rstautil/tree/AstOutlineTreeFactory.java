884eb66116485f5eac6463f19f8c4bd9962a3621
elieserdejesus <elieserdejesus@gmail.com>
2016-09-20 16:25:10 -0300

    Atualizando PS para refletir as mudanças feitas no nucleo

--- src/br/univali/ps/ui/rstautil/tree/AstOutlineTreeFactory.java
+++ src/br/univali/ps/ui/rstautil/tree/AstOutlineTreeFactory.java
@@ -33,6 +33,7 @@ import br.univali.portugol.nucleo.asa.NoOperacaoBitwiseOu;
 import br.univali.portugol.nucleo.asa.NoOperacaoBitwiseRightShift;
 import br.univali.portugol.nucleo.asa.NoOperacaoBitwiseXOR;
 import br.univali.portugol.nucleo.asa.NoOperacaoDivisao;
+import br.univali.portugol.nucleo.asa.NoOperacaoLogica;
 import br.univali.portugol.nucleo.asa.NoOperacaoLogicaDiferenca;
 import br.univali.portugol.nucleo.asa.NoOperacaoLogicaE;
 import br.univali.portugol.nucleo.asa.NoOperacaoLogicaIgualdade;
@@ -530,55 +531,13 @@ class AstOutlineTreeFactory implements VisitanteASA
     }
 
     @Override
-    public Object visitar(NoOperacaoLogicaIgualdade noOperacaoLogicaIgualdade) throws ExcecaoVisitaASA
-    {
-        return null;
-    }
-
-    @Override
-    public Object visitar(NoOperacaoLogicaDiferenca noOperacaoLogicaDiferenca) throws ExcecaoVisitaASA
+    public Object visitar(NoOperacaoLogica noOperacaoLogica) throws ExcecaoVisitaASA
     {
         return null;
     }
 
     @Override
     public Object visitar(NoOperacaoAtribuicao noOperacaoAtribuicao) throws ExcecaoVisitaASA
-    {
-        return null;
-    }
-
-    @Override
-    public Object visitar(NoOperacaoLogicaE noOperacaoLogicaE) throws ExcecaoVisitaASA
-    {
-        return null;
-    }
-
-    @Override
-    public Object visitar(NoOperacaoLogicaOU noOperacaoLogicaOU) throws ExcecaoVisitaASA
-    {
-        return null;
-    }
-
-    @Override
-    public Object visitar(NoOperacaoLogicaMaior noOperacaoLogicaMaior) throws ExcecaoVisitaASA
-    {
-        return null;
-    }
-
-    @Override
-    public Object visitar(NoOperacaoLogicaMaiorIgual noOperacaoLogicaMaiorIgual) throws ExcecaoVisitaASA
-    {
-        return null;
-    }
-
-    @Override
-    public Object visitar(NoOperacaoLogicaMenor noOperacaoLogicaMenor) throws ExcecaoVisitaASA
-    {
-        return null;
-    }
-
-    @Override
-    public Object visitar(NoOperacaoLogicaMenorIgual noOperacaoLogicaMenorIgual) throws ExcecaoVisitaASA
     {
         return null;
     }
