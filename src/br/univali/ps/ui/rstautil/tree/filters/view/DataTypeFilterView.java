4df2cb14a1279b2849692cebb438e99d93bb7e86
AlissonSteffens <alisson.steffens@hotmail.com>
2016-09-27 15:02:59 -0300

    Data Type

--- src/br/univali/ps/ui/rstautil/tree/filters/view/DataTypeFilterView.java
+++ src/br/univali/ps/ui/rstautil/tree/filters/view/DataTypeFilterView.java
@@ -7,8 +7,6 @@ import br.univali.ps.ui.rstautil.tree.filters.DataTypeFilter;
 import br.univali.ps.ui.rstautil.tree.filters.DataTypeFilterListener;
 import br.univali.ps.ui.utils.FabricaDicasInterface;
 import br.univali.ps.ui.weblaf.WeblafUtils;
-import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
