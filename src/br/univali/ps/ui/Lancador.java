ffc0832d8c1d225e27c18fcec5aed0013b90d160
elieserdejesus <elieserdejesus@gmail.com>
2017-05-26 09:23:05 -0300

    Removendo texto padrão gerado pelo NetBeans

--- src/br/univali/ps/ui/Lancador.java
+++ src/br/univali/ps/ui/Lancador.java
@@ -14,14 +14,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- *
  * @author lite
  */
 public class Lancador {
