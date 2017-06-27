e470940d475c227bf68c682cf40d938d99abf318
AdsonEsteves <shinadson@gmail.com>
2017-06-27 15:31:56 -0300

    Revertendo ordem dos arquivos recentes ( fix #241 )

--- src/br/univali/ps/ui/paineis/PainelExemplos.java
+++ src/br/univali/ps/ui/paineis/PainelExemplos.java
@@ -33,6 +33,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 import java.util.Queue;
@@ -123,7 +124,8 @@ public class PainelExemplos extends javax.swing.JPanel implements Themeable{
     
     public void atualizarRecentes(){
         boolean arquivoRemovido = false;
-        Queue<File> files = PortugolStudio.getInstancia().getRecentFilesQueue();
+        List<File> files = new ArrayList<>(PortugolStudio.getInstancia().getRecentFilesQueue());
+        Collections.reverse(files);
         Icon icone = imagemPastaPadrao;
         areaRecentes.removeAll();
         areaLogo.removeAll();
