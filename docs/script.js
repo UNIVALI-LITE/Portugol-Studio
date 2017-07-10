6f828c6fd7ac819d24738ba9f883987e404d661c
AlissonSteffens <alisson.steffens@hotmail.com>
2017-07-10 16:04:52 -0300

    many resolutions

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,23 @@
+var is = false
+var op = 1
 $(document).ready(function(){
   var height = $( window ).height()
   $('section').css('min-height',height)
+  swapC()
 })
 
-$(".arrow").click(function(){
+function swapC() {
+    $('.arrow').animate({ opacity: op }, 1000)
+    if(is){
+      op = 1
+    }else{
+      op = 0.5
+    }
+    is = !is
+    window.setTimeout(function() { swapC() }, 1000)
+}
+
+$(".goto").click(function(){
   var height = $(this.dataset.to).offset().top
   console.log(this.dataset.to + ' - ' + height)
   $("html, body").animate({ scrollTop:  height}, "ease")
