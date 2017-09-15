b294aea3355e94b170b95bdf9a859ed4bff65f07
Adsu <shinadson@gmail.com>
2017-09-15 15:45:01 -0300

    :gem: Criando mecanismo de notificação de atualização (#338) Fix #280
    * :art: Adicionando tela de atualizacao
    
    * :sparkles:Fazendo requisição com a API do Github
    
    * :bug: Corrigindo título da janela que estava incorreto
    
    * :bug: Removendo print de HTML que estava saindo no console
    
    * :gem: Atualizando layout da tela de atualizações
    
    * :gem: Recuperando valor da versão atual e nova versão do PS
    
    Alterando também a cor de fundo do botão de baixar ao passar o mouse sobre ele.
    
    * :gem: Recuperando valor da versão atual e nova versão do PS
    
    Corrigindo scroll para não descer automaticamente. Alterando também a cor de fundo do botão de baixar ao passar o mouse sobre ele.
    
    * :art: Ajustando cores para tema azul

--- ide/src/main/java/br/univali/ps/ui/telas/TelaPrincipal.java
+++ ide/src/main/java/br/univali/ps/ui/telas/TelaPrincipal.java
@@ -15,8 +15,8 @@ import br.univali.ps.ui.abas.AbaCodigoFonte;
 import br.univali.ps.ui.abas.AbaInicial;
 import br.univali.ps.ui.paineis.PainelTabuladoPrincipal;
 import br.univali.ps.ui.swing.ColorController;
-import br.univali.ps.ui.utils.FabricaDicasInterface;
 import br.univali.ps.ui.utils.FileHandle;
+import java.awt.Dimension;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
@@ -24,16 +24,21 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
-import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
 
 /**
  *
@@ -151,7 +156,23 @@ public class TelaPrincipal extends javax.swing.JPanel
         abrirArquivosCodigoFonte(arquivosIniciais);
 
 //        exibirErrosPluginsBibliotecas();
-        exibirLogAtualizacoes();
+        Thread thread = new Thread(){
+            public void run(){
+                JSONObject ultimaVersao = procurarAtualizacoes();
+                if(ultimaVersao!=null)
+                {
+                    String versao = ultimaVersao.getString("tag_name");
+                    String texto = ultimaVersao.getString("body");
+                    if(!versao.equals("v"+PortugolStudio.getInstancia().getVersao()))
+                    {
+                        exibirTelaAtualizacoes(texto, versao);
+                    }
+                }
+            }
+        };
+
+        thread.start();
+        
 
         //baixarNovasAtualizacoes();
     }
@@ -167,28 +188,38 @@ public class TelaPrincipal extends javax.swing.JPanel
             telaErrosPluginsBibliotecas.setVisible(true);
         }
     }
-
-    private void exibirLogAtualizacoes()
+    
+    private JSONObject procurarAtualizacoes()
     {
-        File logAtualizacoes = Configuracoes.getInstancia().getCaminhoLogAtualizacoes();
-
-        if (logAtualizacoes.exists())
-        {
-            try
-            {
-                String atualizacoes = FileHandle.open(logAtualizacoes);
-                TelaLogAtualizacoes telaLogAtualizacoes = new TelaLogAtualizacoes();
-
-                telaLogAtualizacoes.setAtualizacoes(atualizacoes);
-                telaLogAtualizacoes.setVisible(true);
-
-                FileUtils.deleteQuietly(logAtualizacoes);
-            }
-            catch (Exception excecao)
-            {
-                LOGGER.log(Level.SEVERE, "Erro ao carregar o log de atualizações", excecao);
-            }
+        String requestURL = Configuracoes.getInstancia().getUriAtualizacao();
+        try {
+            InputStream is = new URL(requestURL).openStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = org.apache.commons.io.IOUtils.toString(rd);
+            JSONObject json = new JSONObject(jsonText);
+            is.close();
+            return json;
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(TelaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TelaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        return null;
+    }
+
+    private void exibirTelaAtualizacoes(String body, String versao)
+    {
+        SwingUtilities.invokeLater(() -> {
+            TelaCustomBorder main = new TelaCustomBorder("Atualização Encontrada");
+            TelaAtualizacoes ta = new TelaAtualizacoes(body, versao);
+            main.setMinimumSize(new Dimension(280, 450));
+            main.setPanel(ta, false);
+            main.setLocationRelativeTo(null);
+            main.setVisible(true);
+            main.pack();
+        });
+        
     }
 
     public void criarNovoCodigoFonte()
