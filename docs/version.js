0ee4d4f006aec33003569edf737113d3d76ecfa9
Adsu <shinadson@gmail.com>
2017-12-15 16:39:01 -0200

    atualizando versões

--- docs/version.js
+++ docs/version.js
@@ -1,11 +1,11 @@
 var versionInfo = {
     "links": {
-        "windows": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/download/v2.6.4/portugol-studio-2.6.4-windows.exe",
-        "osx": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/download/v2.6.4/portugol-studio-2.6.4-osx.dmg",
-        "linux": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/download/v2.6.4/portugol-studio-2.6.4-linux.run.zip",
-        "linux_x64": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/download/v2.6.4/portugol-studio-2.6.4-linux-x64.run.zip",
-        "others": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/tag/v2.6.4"
+        "windows": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/download/v2.6.5/portugol-studio-2.6.5-windows.exe",
+        "osx": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/download/v2.6.5/portugol-studio-2.6.5-osx.dmg",
+        "linux": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/download/v2.6.5/portugol-studio-2.6.5-linux.run.zip",
+        "linux_x64": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/download/v2.6.5/portugol-studio-2.6.5-linux-x64.run.zip",
+        "others": "https://github.com/UNIVALI-LITE/Portugol-Studio/releases/tag/v2.6.5"
     },
-    "versionString": "2.6.4",
-    "releaseDate": 1510333458371
+    "versionString": "2.6.5",
+    "releaseDate": 1513360951726
 }
