ae6e28f3c50e0d5c4b2cfacdece44d8a85bcf6f4
Manoel Campos da Silva Filho <manoelcampos@gmail.com>
2018-01-29 14:22:50 -0300

    :memo: Melhorar a documentação de pacotes convertendo arquivos package.html para package-info.java (Closes #456) (#462)
    * :art: Organização do código
    
    - Remove mensagens "To change this template, choose Tools | Templates"
      criadas pelo NetBeans no topo dos arquivos.
    - Remove inúmeros imports não utilizados.
    
    * Atualiza .gitignore para ignorar arquivos do IntelliJ IDEA
    
    * :art: Closes #437
    
    * :gem: Inclui novo método geraImportacaoPara na classe GeradorCodigoJava
          para permitir importar todas as classes de um pacote.
          Isto foi necessário depois de movidas as inner classes
          de uma classe para dentro de um novo pacote.
          Antes, bastava importar a classe principal que
          já se tinha acesso às inner classes.
          Agora como precisa importar estas classes que deixaram
          de ser inner classes, o mais fácil é fazer um import pacote.*
          no pacote da classe principal.
    
    * Atualiza .gitignore para ignorar diretórios temporários out gerados pelos testes
    
    * :green_heart: Corrige testes falhos.
    
    - Altera método GeradorCodigoJava.gera() para importar todas as classes
      do novo pacote br.univali.portugol.nucleo.programa
      (classes que antes eram inner classes)
    - Altera arquivos *.javatest para também importar todas
      as classes de tal pacote.
    
    * Removido TODO da classe PortugolLexer pois ela é gerada automaticamente pelo ANTLR
    
    * Updates #437
    
    Corrige import da classe Programa (que mudou de pacote no módulo
    portugol-core e que não foram automaticamente corrigidas durante
    a refatoração), dentro do diretório Portugol-Studio/checker.
    
    * :memo: Melhorar a documentação de pacotes convertendo arquivos package.html
    para package-info.java (Closes #456)

---
+++ core/src/main/java/br/univali/portugol/nucleo/analise/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Este pacote contém todas as classes envolvidas no parsing do código fonte e
+ * nas análises sintática e semântica.
+*/
+package br.univali.portugol.nucleo.analise;
---
+++ core/src/main/java/br/univali/portugol/nucleo/analise/semantica/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Este pacote contém as classes responsáveis por realizar a análise semântica
+ * do código fonte.
+*/
+package br.univali.portugol.nucleo.analise.semantica;
