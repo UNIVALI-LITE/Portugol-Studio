2b9f705ab88b6ac9e37cd67f92b81af8395e6c1a
Adsu <shinadson@gmail.com>
2018-02-05 17:10:34 -0200

    :bug: corrigindo erros de atribuição em bibliotecas (fix #152)

---
+++ core/src/main/java/br/univali/portugol/nucleo/analise/semantica/erros/ErroAliasInexistente.java
@@ -0,0 +1,29 @@
+package br.univali.portugol.nucleo.analise.semantica.erros;
+
+import br.univali.portugol.nucleo.asa.TrechoCodigoFonte;
+import br.univali.portugol.nucleo.bibliotecas.base.MetaDadosBiblioteca;
+import br.univali.portugol.nucleo.bibliotecas.base.MetaDadosConstante;
+import br.univali.portugol.nucleo.mensagens.ErroSemantico;
+
+/**
+ *
+ * @author fillipi
+ */
+public class ErroAliasInexistente extends ErroSemantico {
+    private final String alias;
+
+    public ErroAliasInexistente(TrechoCodigoFonte trechoCodigoFonte, String alias)
+    {
+        super(trechoCodigoFonte);
+        this.alias = alias;
+    }
+
+    @Override
+    protected String construirMensagem()
+    {
+        return String.format("\"%s.\" pode ser apenas utilizado ao se referir a uma biblioteca como alias, porém nenhuma biblioteca o utiliza como alias. \n"+
+                                "Para utiliza-lo como alias, defina uma biblioteca para ele.\n"+
+                                "Exemplo: inclua biblioteca Graficos --> \"%s\"", alias, alias);
+    }
+    
+}
