3fa0794b601dc5e18da7540ad8084f964c5a0b32
AlissonSteffens <alisson.steffens@hotmail.com>
2018-02-07 16:03:19 -0200

    atualizando autoria de funções

--- core/src/main/java/br/univali/portugol/nucleo/analise/semantica/erros/ErroAliasInexistente.java
+++ core/src/main/java/br/univali/portugol/nucleo/analise/semantica/erros/ErroAliasInexistente.java
@@ -1,13 +1,11 @@
 package br.univali.portugol.nucleo.analise.semantica.erros;
 
 import br.univali.portugol.nucleo.asa.TrechoCodigoFonte;
-import br.univali.portugol.nucleo.bibliotecas.base.MetaDadosBiblioteca;
-import br.univali.portugol.nucleo.bibliotecas.base.MetaDadosConstante;
 import br.univali.portugol.nucleo.mensagens.ErroSemantico;
 
 /**
  *
- * @author fillipi
+ * @author Adson
  */
 public class ErroAliasInexistente extends ErroSemantico {
     private final String alias;
--- core/src/main/java/br/univali/portugol/nucleo/analise/semantica/erros/ErroAtribuirFuncaoBiblioteca.java
+++ core/src/main/java/br/univali/portugol/nucleo/analise/semantica/erros/ErroAtribuirFuncaoBiblioteca.java
@@ -2,12 +2,11 @@ package br.univali.portugol.nucleo.analise.semantica.erros;
 
 import br.univali.portugol.nucleo.asa.TrechoCodigoFonte;
 import br.univali.portugol.nucleo.bibliotecas.base.MetaDadosBiblioteca;
-import br.univali.portugol.nucleo.bibliotecas.base.MetaDadosConstante;
 import br.univali.portugol.nucleo.mensagens.ErroSemantico;
 
 /**
  *
- * @author fillipi
+ * @author Adson
  */
 public class ErroAtribuirFuncaoBiblioteca extends ErroSemantico {
     private final MetaDadosBiblioteca metaDadosBiblioteca;
