09faff557c4d71fe9f4540061e77abed1833ac56
Gabriel Schade <re035148@ITAW10N07304>
2018-04-04 22:02:09 -0300

    Incremento na biblioteca para requisições customizadas.

--- core/src/main/java/br/univali/portugol/nucleo/bibliotecas/ServicosWeb.java
+++ core/src/main/java/br/univali/portugol/nucleo/bibliotecas/ServicosWeb.java
@@ -12,9 +12,13 @@ import br.univali.portugol.nucleo.bibliotecas.base.anotacoes.Autor;
 import br.univali.portugol.nucleo.bibliotecas.base.anotacoes.DocumentacaoFuncao;
 import br.univali.portugol.nucleo.bibliotecas.base.anotacoes.DocumentacaoParametro;
 import br.univali.portugol.nucleo.bibliotecas.base.anotacoes.DocumentacaoBiblioteca;
+import br.univali.portugol.nucleo.bibliotecas.base.anotacoes.DocumentacaoConstante;
 import br.univali.portugol.nucleo.bibliotecas.base.anotacoes.PropriedadesBiblioteca;
 import br.univali.portugol.nucleo.bibliotecas.web.RestClient;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -28,6 +32,35 @@ import java.io.IOException;
 public final class ServicosWeb extends Biblioteca{
     public ServicosWeb()
     {    }
+
+    private HttpURLConnection obterConexaoEmCache() throws ErroExecucaoBiblioteca{
+        HttpURLConnection conexao = RestClient.obterConexaoEmCache();
+        if(conexao == null)
+            throw new ErroExecucaoBiblioteca("É necessário criar uma conexão customizada antes de adicionar o cabeçalho");
+        
+        return conexao;
+    }
+
+    @DocumentacaoConstante(
+            descricao = "Constante para definir o método HTTP GET",
+            referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#GET"
+    )
+    public static final int OBTER = 1;
+    @DocumentacaoConstante(
+            descricao = "Constante para definir o método HTTP POST",
+            referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#POST"
+    )
+    public static final int PUBLICAR = 2;
+    @DocumentacaoConstante(
+            descricao = "Constante para definir o método HTTP PUT",
+            referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#PUT"
+    )
+    public static final int ATUALIZAR = 3;
+    @DocumentacaoConstante(
+            descricao = "Constante para definir o método HTTP DELETE",
+            referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#DELETE"
+    )
+    public static final int EXCLUIR = 4;
     
     @DocumentacaoFuncao(
             descricao = "Realiza uma chamada HTTP GET para o endereço especificado, retornando uma cadeia no formato JSON como resposta",
@@ -36,7 +69,7 @@ public final class ServicosWeb extends Biblioteca{
                 @DocumentacaoParametro(nome = "endereco", descricao = "endereço para o qual a requisição será feita")
             },
             referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#GET",
-            retorno = "O arquivo JSON contendo a resposta da requisição",
+            retorno = "a cadeia JSON contendo a resposta da requisição",
             autores =
             {
                 @Autor(nome = "Gabriel Schade", email = "gabrielschade@univali.br")
@@ -58,7 +91,7 @@ public final class ServicosWeb extends Biblioteca{
                 @DocumentacaoParametro(nome = "endereco", descricao = "endereço para o qual a requisição será feita")
             },
             referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#DELETE",
-            retorno = "O arquivo JSON contendo a resposta da requisição",
+            retorno = "a cadeia JSON contendo a resposta da requisição",
             autores =
             {
                 @Autor(nome = "Gabriel Schade", email = "gabrielschade@univali.br")
@@ -81,7 +114,7 @@ public final class ServicosWeb extends Biblioteca{
                 @DocumentacaoParametro(nome = "objeto", descricao = "objeto no formato JSON passado por parâmetro no corpo da requisição")
             },
              referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#POST",
-            retorno = "O arquivo JSON contendo a resposta da requisição",
+            retorno = "a cadeia JSON contendo a resposta da requisição",
             autores =
             {
                 @Autor(nome = "Gabriel Schade", email = "gabrielschade@univali.br")
@@ -104,13 +137,13 @@ public final class ServicosWeb extends Biblioteca{
                 @DocumentacaoParametro(nome = "objeto", descricao = "objeto no formato JSON passado por parâmetro no corpo da requisição")
             },
              referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#PUT",
-            retorno = "O arquivo JSON contendo a resposta da requisição",
+            retorno = "a cadeia JSON contendo a resposta da requisição",
             autores =
             {
                 @Autor(nome = "Gabriel Schade", email = "gabrielschade@univali.br")
             }
     )
-    public String colocar_dados(String endereco, String objeto) throws ErroExecucaoBiblioteca, InterruptedException
+    public String atualizar_dados(String endereco, String objeto) throws ErroExecucaoBiblioteca, InterruptedException
     {
         try {
             return RestClient.put(endereco,objeto);
@@ -118,4 +151,97 @@ public final class ServicosWeb extends Biblioteca{
             throw new ErroExecucaoBiblioteca("Ocorreu um problema na requisição.");
         }
     }
+    
+    @DocumentacaoFuncao(
+            descricao = "Abre uma conexão customizável com o endereço especificado.",
+             parametros =
+            {
+                @DocumentacaoParametro(nome = "endereco", descricao = "endereço para o qual a requisição será feita")
+            },
+             referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol",
+            autores =
+            {
+                @Autor(nome = "Gabriel Schade", email = "gabrielschade@univali.br")
+            }
+    )
+    public void abrir_conexao(String endereco) throws ErroExecucaoBiblioteca, InterruptedException{
+        try {
+            RestClient.abrirConexao(endereco);
+        } catch (IOException ex) {
+            throw new ErroExecucaoBiblioteca("Ocorreu um problema na criação da requisição.");
+        }
+    }
+    
+    @DocumentacaoFuncao(
+            descricao = "Adiciona um cabeçalho na conexão customizável, para utilizar esta função é necessário abrir uma conexão antes.",
+             parametros =
+            {
+                @DocumentacaoParametro(nome = "chave", descricao = "chave do cabeçalho que será incluído na conexão"),
+                @DocumentacaoParametro(nome = "valor", descricao = "valor do cabeçalho que será incluído na conexão")
+            },
+             referencia = "https://pt.wikipedia.org/wiki/Lista_de_campos_de_cabe%C3%A7alho_HTTP",
+            autores =
+            {
+                @Autor(nome = "Gabriel Schade", email = "gabrielschade@univali.br")
+            }
+    )
+    public void adicionar_cabecalho(String chave, String valor) throws ErroExecucaoBiblioteca, InterruptedException{
+        HttpURLConnection conexao = obterConexaoEmCache();       
+        RestClient.incluirCabecalhoNaRequisicao( conexao,chave, valor);    
+    }
+    
+    @DocumentacaoFuncao(
+            descricao = "Adiciona um objeto no formato JSON na conexão customizável, para utilizar esta função é necessário abrir uma conexão antes. Esta função deve ser utilizada para informar os parâmetros dos métodos HTTP POST e PUT.",
+            parametros =
+            {
+                @DocumentacaoParametro(nome = "objeto", descricao = "objeto no formato JSON passado por parâmetro no corpo da requisição")
+            },
+             referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol",
+            autores =
+            {
+                @Autor(nome = "Gabriel Schade", email = "gabrielschade@univali.br")
+            }
+    )
+    public void adicionar_parametros(String objeto) throws ErroExecucaoBiblioteca, InterruptedException{       
+        RestClient.adicionarParametroNaConexao(objeto);
+    }
+    
+    @DocumentacaoFuncao(
+            descricao = "Realiza uma chamada HTTP através de uma conexão customizada, retornando uma cadeia no formato JSON como resposta",
+             parametros =
+            {
+                @DocumentacaoParametro(nome = "metodoHttp", descricao = "Método HTTP que será utilizado nesta execução (GET, POST, PUT ou DELETE).")
+            },
+            referencia = "https://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol",
+            retorno = "a cadeia JSON contendo a resposta da requisição",
+            autores =
+            {
+                @Autor(nome = "Gabriel Schade", email = "gabrielschade@univali.br")
+            }
+    )
+    public String fazer_requisicao(int metodoHttp) throws ErroExecucaoBiblioteca, InterruptedException
+    {       
+        try {
+            String retorno = "";
+            switch(metodoHttp){
+                case OBTER:
+                    retorno = RestClient.get(obterConexaoEmCache());
+                    break;
+                case PUBLICAR:
+                    retorno = RestClient.post(obterConexaoEmCache());
+                    break;
+                case ATUALIZAR:
+                    retorno = RestClient.put(obterConexaoEmCache());
+                    break;
+                case EXCLUIR:
+                    retorno = RestClient.delete(obterConexaoEmCache());
+                    break;
+            }
+            return retorno;
+        } catch (IOException ex) {
+            throw new ErroExecucaoBiblioteca("Ocorreu um problema na requisição.");
+        }
+    }
+    
+
 }
--- core/src/main/java/br/univali/portugol/nucleo/bibliotecas/web/RestClient.java
+++ core/src/main/java/br/univali/portugol/nucleo/bibliotecas/web/RestClient.java
@@ -18,13 +18,16 @@ import java.net.URL;
  * @author Gabriel Schade
  */
 public class RestClient {
+
+     
+    private static HttpURLConnection conexaoEmCache = null;
+    private static String parametroJsonEmCache = "";
     
-    private static HttpURLConnection criarConexao(String endereco, String metodoHttp) 
+    private static HttpURLConnection criarConexao(String endereco) 
             throws MalformedURLException, IOException{
         URL url = new URL(endereco);
         
 	HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
-        conexao.setRequestMethod(metodoHttp);
         conexao.setRequestProperty("Accept", "application/json");
         conexao.setRequestProperty("Content-Type", "application/json");
         conexao.setDoOutput(true);
@@ -46,23 +49,13 @@ public class RestClient {
         
         return resposta.toString();
     }
-    
-    private static HttpURLConnection incluirParametrosNaRequisicao(HttpURLConnection conexao, String json) throws IOException
+
+private static String execucaoInternaChamadaServico(HttpURLConnection conexao, String metodo,String json) throws IOException
     {
-        OutputStream outputStream = conexao.getOutputStream();
-        outputStream.write(json.getBytes());
-        outputStream.flush();
-        
-        return conexao;
-    }
-    
-    private static String execucaoInternaChamadaServico(String endereco, String metodo,String json) throws IOException
-    {
-        HttpURLConnection conexao = null;
         int codigoHttp;
         String retorno;
         try {
-		conexao = criarConexao(endereco, metodo);
+                conexao.setRequestMethod(metodo);
                 conexao = json.equals("")?
                         conexao
                         :incluirParametrosNaRequisicao(conexao, json);
@@ -81,20 +74,72 @@ public class RestClient {
         return retorno;
     }
     
+    public static HttpURLConnection incluirCabecalhoNaRequisicao(HttpURLConnection conexao, String chave, String valor){
+        conexao.addRequestProperty(chave, valor);
+        return conexao;
+    }
+    
+    private static HttpURLConnection incluirParametrosNaRequisicao(HttpURLConnection conexao, String json) throws IOException
+    {
+        OutputStream outputStream = conexao.getOutputStream();
+        outputStream.write(json.getBytes());
+        outputStream.flush();
+        
+        return conexao;
+    }
+    
+    
+    
     public static String get(String endereco) throws IOException{
-        return execucaoInternaChamadaServico(endereco,"GET", "");
+        return get(criarConexao(endereco));
     }
     
     public static String post(String endereco, String objeto) throws IOException{
-        return execucaoInternaChamadaServico(endereco,"POST", objeto);
+        return post(criarConexao(endereco),objeto);
     }
     
     public static String put(String endereco, String objeto) throws IOException{
-        return execucaoInternaChamadaServico(endereco,"PUT", objeto);
+        return put(criarConexao(endereco), objeto);
     }
     
     public static String delete(String endereco) throws IOException{
-        return execucaoInternaChamadaServico(endereco,"DELETE","");
+        return delete(criarConexao(endereco));
+    }
+    
+    public static String get(HttpURLConnection conexao) throws IOException{
+        return execucaoInternaChamadaServico(conexao,"GET", "");
+    }
+    
+    public static String post(HttpURLConnection conexao) throws IOException{
+        return execucaoInternaChamadaServico(conexao,"POST", parametroJsonEmCache);
+    }
+    
+    public static String post(HttpURLConnection conexao, String objeto) throws IOException{
+        return execucaoInternaChamadaServico(conexao,"POST", objeto);
+    }
+    
+    public static String put(HttpURLConnection conexao) throws IOException{
+        return execucaoInternaChamadaServico(conexao,"PUT", parametroJsonEmCache);
+    }
+    
+    public static String put(HttpURLConnection conexao, String objeto) throws IOException{
+        return execucaoInternaChamadaServico(conexao,"PUT", objeto);
+    }
+    
+    public static String delete(HttpURLConnection conexao) throws IOException{
+        return execucaoInternaChamadaServico(conexao,"DELETE","");
+    }
+    
+    public static void abrirConexao(String endereco) throws IOException{
+        conexaoEmCache = criarConexao(endereco);
+    }
+
+    public static HttpURLConnection obterConexaoEmCache(){
+        return conexaoEmCache;
+    }
+    
+    public static void adicionarParametroNaConexao(String objeto){
+        parametroJsonEmCache = objeto;
     }
     
 }
