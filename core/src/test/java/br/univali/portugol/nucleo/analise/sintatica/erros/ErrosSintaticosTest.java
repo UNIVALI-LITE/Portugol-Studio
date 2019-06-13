bef5bf831a87a7b1d2643e41d57e09239c3edc0a
elieserdejesus <elieserdejesus@gmail.com>
2019-06-13 08:25:02 -0300

    Adicionando teste para cadeia incompleta

--- core/src/test/java/br/univali/portugol/nucleo/analise/sintatica/erros/ErrosSintaticosTest.java
+++ core/src/test/java/br/univali/portugol/nucleo/analise/sintatica/erros/ErrosSintaticosTest.java
@@ -13,6 +13,25 @@ import org.junit.Test;
 public class ErrosSintaticosTest {
 
     @Test
+    public void testCadeiaIncompleta() throws Exception {
+         String codigoFonte
+                = " programa {                                                  "
+                + "    funcao inicio(){                                         "
+                + "       cadeia teste = \"asd                                  "
+                + "    }                                                        "
+                + " }                                                           ";
+
+      
+        AnalisadorAlgoritmo analisador = new AnalisadorAlgoritmo();
+        ResultadoAnalise analise = analisador.analisar(codigoFonte);
+                
+        Assert.assertEquals(1, analise.getErrosSintaticos().size());
+        
+        ErroSintatico erro = analise.getErrosSintaticos().get(0);
+        Assert.assertTrue(erro instanceof ErroCadeiaIncompleta);
+    }
+    
+    @Test
     public void testCaracterBizarroNaFuncaoInicio() throws Exception {
          String codigoFonte
                 = " programa {                                                  "
