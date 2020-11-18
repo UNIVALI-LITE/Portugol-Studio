64ed882570a2624043787961f5523c497e006d5b
Adsu <shinadson@gmail.com>
2020-11-18 13:36:21 -0300

    Update ErroExpressaoForaEscopoFuncao.java and TradutorMismatchedTokenException.java

--- core/src/main/java/br/univali/portugol/nucleo/analise/sintatica/erros/ErroExpressaoForaEscopoFuncao.java
+++ core/src/main/java/br/univali/portugol/nucleo/analise/sintatica/erros/ErroExpressaoForaEscopoFuncao.java
@@ -2,20 +2,20 @@ package br.univali.portugol.nucleo.analise.sintatica.erros;
 
 import br.univali.portugol.nucleo.mensagens.ErroSintatico;
 
-public class ErroExpressaoInesperada extends ErroSintatico
+public class ErroExpressaoForaEscopoFuncao extends ErroSintatico
 {
     private String token;
     
-    public ErroExpressaoInesperada(int linha, int coluna, String token)
+    public ErroExpressaoForaEscopoFuncao(int linha, int coluna, String token)
     {
-        super(linha, coluna,"ErroSintatico.ErroExpressaoInesperada");
+        super(linha, coluna,"ErroSintatico.ErroExpressaoForaEscopoFuncao");
         this.token = token;
     }
     
     @Override
     protected String construirMensagem()
     {
-        return String.format("A expressão '%s' não era esperada neste local, remova a expressão para corrigir o problema", token);
+        return String.format("A expressão '%s' está fora de um escopo de função e nunca será chamada. Adicione ela a uma função ou remova-a.", token);
     }    
 
     public String getToken() {
